690aa776081e77214db95ddd5c53c7ec3ac79d61
Hassan <hassanismailfawaz@gmail.com>
2019-08-27 10:41:16 +0200

    adding

--- utils/constants.py
+++ utils/constants.py
@@ -19,6 +19,7 @@ UNIVARIATE_DATASET_NAMES = ['50words', 'Adiac', 'ArrowHead', 'Beef', 'BeetleFly'
 UNIVARIATE_DATASET_NAMES = ['Meat', 'Coffee']
 
 UNIVARIATE_ARCHIVE_NAMES = ['TSC', 'InlineSkateXPs', 'SITS']
+UNIVARIATE_ARCHIVE_NAMES = ['TSC']
 
 SITS_DATASETS = ['SatelliteFull_TRAIN_c301', 'SatelliteFull_TRAIN_c200', 'SatelliteFull_TRAIN_c451',
                  'SatelliteFull_TRAIN_c89', 'SatelliteFull_TRAIN_c677', 'SatelliteFull_TRAIN_c59',
